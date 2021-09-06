ebd611295279f1ea2df70716060c023569dcf314
Serhii <malgothsm@gmail.com>
2021-09-06 16:38:14 +0300

    Fixed MatrixPrint

--- exerciseweek2/exercise.java
+++ exerciseweek2/exercise.java
@@ -69,17 +69,24 @@ public class exercise {
 	
 	public static void main(String[] args){
 
-		int data[] = { 3, 6, 7, 10, 34, 56, 60 };
-		int numberToFind = 10;
-
-		int number = -1;
-			
-			  for (int i = 0; i < data.length; i++) {
-		            if ( data[i] == numberToFind) {
-		            	 number = i;
-		                break;
-		            }
-		        }
-		        System.out.println(number);
-}
+		for (int j = 1; j <= 25; j++) {
+			switch (j) {
+				case 1:
+				case 5:
+				case 7:
+				case 9:
+				case 13:
+				case 17:
+				case 19:
+				case 21:
+				case 25:
+					System.out.print(" * ");
+					break;
+				default: System.out.printf("%2d ", j);
+			}
+			if (( j % 5) == 0) {
+				System.out.print("\n");
+			}
+		}
+   }
 }
